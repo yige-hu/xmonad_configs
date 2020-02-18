3d0351a3e0b2c84e1c62a3f6c607cd9a81f4b10b
Yige Hu <yige@cs.utexas.edu>
2020-02-17 18:45:55 -0600

    1. XMonad.Actions.Volume failed to load since xmonad-extras failed to install. Fixed by removing the alert sounds when changing the volumn.
    2. Add key-binding to xscreensaver, now Window+shift+L launches the
    screen saver and locks the screen.

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import XMonad
-import XMonad.Actions.Volume
+--import XMonad.Actions.Volume   -- cannot install xmonad-extras
 import XMonad.Util.Dzen
 import Data.Map    (fromList)
 import Data.Monoid (mappend)
@@ -37,15 +37,23 @@ yigeConfig = do
 		-- mute button
 		((0, 0x1008FF12), spawn "pulse-volume.sh toggle")
 		-- volumeup button
-		,((0, 0x1008FF13), spawn "pulse-volume.sh increase" >> getVolume >>= alert)
+--		,((0, 0x1008FF13), spawn "pulse-volume.sh increase" >> getVolume >>= alert)
+		,((0, 0x1008FF13), spawn "pulse-volume.sh increase")
 		-- volumedown button
-		,((0, 0x1008FF11), spawn "pulse-volume.sh decrease" >> getVolume >>= alert)
+--		,((0, 0x1008FF11), spawn "pulse-volume.sh decrease" >> getVolume >>= alert)
+		,((0, 0x1008FF11), spawn "pulse-volume.sh decrease")
+
+		-- keybinding (Shift + Meta + L) is for locking the screen
+		-- put "xscreensaver -no-splash &" in ~/.xsessionrc to run xscreensaver daemon when loading xserver
+		, ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock; xset dpms force off")
+		, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+		, ((0, xK_Print), spawn "scrot")
 		]
 		}
 	where
 
 -- xmobar
-	myDynLog	h = dynamicLogWithPP defaultPP
+	myDynLog	h = dynamicLogWithPP $ defaultPP
 			{ ppCurrent = xmobarColor "yellow" "" . wrap "[" "]"
 			, ppTitle   = xmobarColor "green"  "" . shorten 40
 			, ppVisible = wrap "(" ")"
